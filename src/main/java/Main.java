f6b756d1decd5ad7a214fcd39f5e735c55003705
Johan Rusvik <johan.rusvik@nav.no>
2017-12-06 08:24:19 +0100

    FO-51 Setter ssl props under oppstart

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import no.nav.fo.veilarbjobbsokerkompetanse.ApplicationConfig;
 import static java.lang.System.setProperty;
 import static no.nav.brukerdialog.security.context.SubjectHandler.SUBJECTHANDLER_KEY;
 import static no.nav.modig.security.sts.utility.STSConfigurationUtility.STS_URL_KEY;
+import static no.nav.sbl.util.EnvironmentUtils.getOptionalProperty;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 public class Main {
@@ -12,8 +13,20 @@ public class Main {
     public static final String SECURITYTOKENSERVICE_URL = "SECURITYTOKENSERVICE_URL";
     public static final String SRVVEILARBJOBBSOKERKOMPETANSE_USERNAME = "SRVVEILARBJOBBSOKERKOMPETANSE_USERNAME";
     public static final String SRVVEILARBJOBBSOKERKOMPETANSE_PASSWORD = "SRVVEILARBJOBBSOKERKOMPETANSE_PASSWORD";
+    public static final String TRUSTSTORE = "javax.net.ssl.trustStore";
+    public static final String TRUSTSTOREPASSWORD = "javax.net.ssl.trustStorePassword";
+    public static final String APP_TRUSTSTORE_PASSWORD = "APP_TRUSTSTORE_PASSWORD";
 
     public static void main(String... args) throws Exception {
+        if (getOptionalProperty(TRUSTSTORE) == null) {
+            System.setProperty(TRUSTSTORE, "/var/run/secrets/naisd.io/app_truststore_keystore");
+        }
+        if (getOptionalProperty(TRUSTSTOREPASSWORD) == null) {
+            if (getRequiredProperty(APP_TRUSTSTORE_PASSWORD) == null) {
+                throw new IllegalStateException("ENV APP_TRUSTSTORE_PASSWORD was not supplied");
+            }
+            System.setProperty(TRUSTSTOREPASSWORD, getRequiredProperty(APP_TRUSTSTORE_PASSWORD));
+        }
         setProperty(STS_URL_KEY, getRequiredProperty(SECURITYTOKENSERVICE_URL));
         setProperty("no.nav.modig.security.systemuser.username", getRequiredProperty(SRVVEILARBJOBBSOKERKOMPETANSE_USERNAME));
         setProperty("no.nav.modig.security.systemuser.password", getRequiredProperty(SRVVEILARBJOBBSOKERKOMPETANSE_PASSWORD));
