e489e60d7a61e054e804d6cd3b28dbfc961b85dc
Johan Rusvik <johan.rusvik@nav.no>
2017-12-20 15:22:50 +0100

    FO-51 Tester sekvensen som generer id

--- src/main/java/no/nav/fo/veilarbjobbsokerkompetanse/db/JobbsokerKartleggingDAO.java
+++ src/main/java/no/nav/fo/veilarbjobbsokerkompetanse/db/JobbsokerKartleggingDAO.java
@@ -64,7 +64,7 @@ public class JobbsokerKartleggingDAO {
         );
     }
 
-    private long getNextUniqueJobbsokerkompetanseId() {
+    long getNextUniqueJobbsokerkompetanseId() {
         return nesteFraSekvens("JOBBSOKERKOMPETANSE_ID_SEQ");
     }
 
--- src/test/java/no/nav/fo/veilarbjobbsokerkompetanse/db/JobbsokerKartleggingDAOTest.java
+++ src/test/java/no/nav/fo/veilarbjobbsokerkompetanse/db/JobbsokerKartleggingDAOTest.java
@@ -8,7 +8,9 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.After;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.Timestamp;
@@ -21,6 +23,7 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.Matchers.equalTo;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JobbsokerKartleggingDAOTest extends IntegrasjonsTest {
 
     private JdbcTemplate database = getBean(JdbcTemplate.class);
@@ -29,6 +32,21 @@ public class JobbsokerKartleggingDAOTest extends IntegrasjonsTest {
     @After
     public void deleteTestData() {
         database.update("DELETE FROM " + JOBBSOKERKOMPETANSE);
+    }
+
+    @Test
+    public void aaaaa_id_generator_leverer_korrekt_id() {
+        long en = jobbsokerKartleggingDAO.getNextUniqueJobbsokerkompetanseId();
+        long to = jobbsokerKartleggingDAO.getNextUniqueJobbsokerkompetanseId();
+        long tre = jobbsokerKartleggingDAO.getNextUniqueJobbsokerkompetanseId();
+        long fire = jobbsokerKartleggingDAO.getNextUniqueJobbsokerkompetanseId();
+        long fem = jobbsokerKartleggingDAO.getNextUniqueJobbsokerkompetanseId();
+        long seks = jobbsokerKartleggingDAO.getNextUniqueJobbsokerkompetanseId();
+        long syv = jobbsokerKartleggingDAO.getNextUniqueJobbsokerkompetanseId();
+
+        assertThat(en, equalTo(1L));
+        assertThat(fire, equalTo(4L));
+        assertThat(syv, equalTo(7L));
     }
 
     @Test
