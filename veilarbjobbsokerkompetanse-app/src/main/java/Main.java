aed729dae199b15049cc75ba38861b9ed3223b94
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2018-03-07 14:51:20 +0100

    FO-680: Get aktoer_endpoint_url fron env instead of property

--- veilarbjobbsokerkompetanse-app/src/main/java/Main.java
+++ veilarbjobbsokerkompetanse-app/src/main/java/Main.java
@@ -1,15 +1,15 @@
 import no.nav.apiapp.ApiApp;
-import no.nav.dialogarena.aktor.AktorConfig;
 import no.nav.fo.veilarbjobbsokerkompetanse.ApplicationConfig;
 
-import static java.lang.System.getProperty;
+import static java.lang.System.getenv;
 import static java.lang.System.setProperty;
+import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.fo.veilarbjobbsokerkompetanse.ApplicationConfig.AKTOER_V2_ENDPOINTURL;
 
 public class Main {
 
     public static void main(String... args) throws Exception {
-        setProperty(AktorConfig.AKTOER_ENDPOINT_URL, getProperty(AKTOER_V2_ENDPOINTURL));
+        setProperty(AKTOER_ENDPOINT_URL, getenv(AKTOER_V2_ENDPOINTURL));
         ApiApp.startApp(ApplicationConfig.class, args);
     }
 
