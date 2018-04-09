f5e01c0baf20852ca3dfcb99bd07a26984dbcc2c
Jan Erik Robertsen <jan.erik.robertsen@nav.no>
2018-04-09 16:00:57 +0200

    FO-685: Integration with veilarboppfolging and /me endpoint

---
+++ src/main/java/no/nav/fo/veilarbjobbsokerkompetanse/client/OppfolgingStatus.java
@@ -0,0 +1,11 @@
+package no.nav.fo.veilarbjobbsokerkompetanse.client;
+
+import lombok.Data;
+
+@Data
+public class OppfolgingStatus {
+
+    private String fnr;
+    private boolean underOppfolging;
+
+}
--- src/main/java/no/nav/fo/veilarbjobbsokerkompetanse/provider/JobbsokerKartleggingRS.java
+++ src/main/java/no/nav/fo/veilarbjobbsokerkompetanse/provider/JobbsokerKartleggingRS.java
@@ -1,8 +1,8 @@
 package no.nav.fo.veilarbjobbsokerkompetanse.provider;
 
-import no.nav.apiapp.feil.Feil;
-import no.nav.dialogarena.aktor.AktorService;
+import no.nav.fo.veilarbjobbsokerkompetanse.client.OppfolgingClient;
 import no.nav.fo.veilarbjobbsokerkompetanse.provider.domain.BesvarelseDto;
+import no.nav.fo.veilarbjobbsokerkompetanse.provider.domain.MeDto;
 import no.nav.fo.veilarbjobbsokerkompetanse.service.BesvarelseService;
 import no.nav.modig.core.context.SubjectHandler;
 import org.springframework.stereotype.Component;
@@ -12,41 +12,41 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import java.util.function.Supplier;
 
-import static no.nav.apiapp.feil.Feil.Type.FINNES_IKKE;
 import static no.nav.fo.veilarbjobbsokerkompetanse.Mapper.map;
 
 @Component
-@Path("/jobbsokerkartlegging")
+@Path("/")
 @Produces("application/json")
 public class JobbsokerKartleggingRS {
-
-    private static final Supplier<Feil> FANT_IKKE_AKTOR =
-            () -> new Feil(FINNES_IKKE, "Finner ikke aktørId for gitt Fnr");
-
-    @Inject
-    private AktorService aktorService;
 
     @Inject
     private BesvarelseService besvarelseService;
 
+    @Inject
+    private OppfolgingClient oppfolgingClient;
+
     @GET
+    @Path("me")
+    public MeDto aboutMe() {
+        return new MeDto().setUnderOppfolging(oppfolgingClient.underOppfolging(getFnr()));
+    }
+
+    @GET
+    @Path("hent")
     public BesvarelseDto hentNyesteBesvarelseForAktor() {
-        return map(besvarelseService.fetchMostRecentByAktorId(getAktorId()));
+        return map(besvarelseService.fetchMostRecentByFnr(getFnr()));
     }
 
     @POST
-    @Path("/opprett")
+    @Path("opprett")
     public BesvarelseDto opprettBesvarelse(BesvarelseDto besvarelseDto) {
-        String aktorId = getAktorId();
-        besvarelseService.create(aktorId, map(besvarelseDto));
-        return map(besvarelseService.fetchMostRecentByAktorId(aktorId));
+        besvarelseService.create(getFnr(), map(besvarelseDto));
+        return map(besvarelseService.fetchMostRecentByFnr(getFnr()));
     }
 
-    private String getAktorId() {
-        String fnr = SubjectHandler.getSubjectHandler().getUid();
-        return aktorService.getAktorId(fnr).orElseThrow(FANT_IKKE_AKTOR);
+    private String getFnr() {
+        return SubjectHandler.getSubjectHandler().getUid();
     }
 
 }
